8e9ad7f8f8c865fe7e93fb8a60ecc58c181a4766
ahmadshaikh <38271272+ahmadshaikh@users.noreply.github.com>
2021-08-16 16:05:31 +0530

    Add files via upload

---
+++ video call/Scripts/ngrok-script.py
@@ -0,0 +1,12 @@
+#!c:\users\shaik\appdata\local\programs\python\python37\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pyngrok==4.1.5','console_scripts','ngrok'
+__requires__ = 'pyngrok==4.1.5'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pyngrok==4.1.5', 'console_scripts', 'ngrok')()
+    )
